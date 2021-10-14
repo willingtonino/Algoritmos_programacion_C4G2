2f1901b042306d69fff016dd3c93d82f324e4e08
Willington Niño <wninope22001@universidadean.edu.co>
2021-10-14 16:48:27 -0500

    actualización
    Ejercicio_10 y taller_secuencia actualizado

--- Taller_secuencias_de_control/Ejercicio_10.py
+++ Taller_secuencias_de_control/Ejercicio_10.py
@@ -14,10 +14,10 @@ Ca=float(input("Digite la cantidad de Chelines austríacos: "))
 Dg=float(input("Digite la cantidad de Dracmas griegos: "))
 Pe=float(input("Digite la cantidad de Pesetas: "))
 #Caja negra
-Pse=(956871/100)*Ca#Float
-ff=Dg*(88607/100)/20110#Float
-dl=Pe/122499#Float
-li=(100/9289)*Pe#Float
+Pse=(956.871/100)*Ca#Float
+ff=Dg*(88.607/100)/20.110#Float
+dl=Pe/122.499#Float
+li=(100/9.289)*Pe#Float
 #Salidas
 print(Ca,"chelines austriacos son",Pse,"pesetas")
 print(Dg,"dracmas griegos son",ff,"francos franceses")
