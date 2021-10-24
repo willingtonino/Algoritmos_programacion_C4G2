1eb1880ad737915c67fc68bf07b3df7058068d0f
Willington Niño <wninope22001@universidadean.edu.co>
2021-10-24 17:18:52 -0500

    Delete date.py

--- Taller_secuencias_control_secuencial/date.py
+++
@@ -1,6 +0,0 @@
-from datetime import date
-
-today=date.today()
-print ("El día actual es {}".format(today.day))
-print ("El mes actual es {}".format(today.month))
-print ("El año actual es {}".format(today.year))
