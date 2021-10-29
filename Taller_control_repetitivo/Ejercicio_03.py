5ef3e9ef1e793c74224f52b2573ca591ec26c023
Willington Niño <91491810+willingtonino@users.noreply.github.com>
2021-10-29 12:53:29 -0500

    Delete Taller_control_repetitivo directory

--- Taller_control_repetitivo/Ejercicio_03.py
+++
@@ -1,7 +0,0 @@
-par=0
-#Caja negra
-for i in range(97,1004):
-    if(i%2==0):
-        par=par+i
-#Salida
-print(par)
--- Taller_control_repetitivo/Ejercicio_04.py
+++
@@ -1,7 +0,0 @@
-contador=6
-suma=6
-for i in range(0,11):
-        contador=contador+5
-        suma=suma+contador
-print(contador)
-print(suma)
